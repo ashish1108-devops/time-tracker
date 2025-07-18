653ab167e70a084e08bce6d1f113510f86a2e9d5
ashish1108-devops <garg.ashish1108@gmail.com>
2025-07-19 00:55:11 +0530

    Update TrackerCoreConfigTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -1,9 +1,5 @@
 package training.taylor.timetracker.core;
 
-import static org.junit.Assert.*;
-
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +8,20 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TrackerCoreConfig.class)
 public class TrackerCoreConfigTest {
 
     @Autowired
     @Qualifier("timesheet")
-    List<TimeEntry> entries;
+    private List<TimeEntry> entries;
 
     @Test
-    public void testMe() {
-        assertNotNull(entries);
+    public void testBeanNotNull() {
+        assertNotNull("The timesheet bean should not be null", entries);
     }
 }
